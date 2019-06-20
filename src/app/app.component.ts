7aae178c5bb6100e07efb14664879dcaec946eb8
Karson Braaten <karsonbraaten@gmail.com>
2019-06-20 14:07:39 -0600

    back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy
+} from '@angular/common'
 import { Component, ViewChild } from '@angular/core'
+
 import { NavigationAction } from '@meld/app-bar'
 import { ScaffoldComponent } from '@meld/scaffold'
 
 @Component({
+  providers: [
+    Location,
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
@@ -10,18 +20,20 @@ import { ScaffoldComponent } from '@meld/scaffold'
 export class AppComponent {
   @ViewChild(ScaffoldComponent, { static: true }) scaffold: ScaffoldComponent
 
+  constructor(private location: Location) {}
+
   onFilter() {
     this.scaffold.toggleSideSheet()
   }
 
   onNavigate(action: NavigationAction) {
-    console.log(action)
     switch (action) {
       case 'menu':
         this.scaffold.toggleDrawer()
       case 'close':
         break
       case 'back':
+        this.location.back()
         break
     }
   }
